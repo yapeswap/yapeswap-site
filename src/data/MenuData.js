dcd745141c323c42d891eb03862cc706bdfbd7a0
stewiezoolights <84042366+stewiezoolights@users.noreply.github.com>
2021-08-17 13:05:07 -0500

    Forum link

--- src/data/MenuData.js
+++ src/data/MenuData.js
@@ -1,6 +1,6 @@
 export const menuData = [
     {title: "Doc", link: "https://docs.yape.exchange/"},
     {title: "DAO", link: "https://dao.yape.exchange/"},
-    {title: "Forum", link: "https://forum.workhard.finance/"},
+    {title: "Forum", link: "https://forum.yape.exchange/"},
     
 ]
