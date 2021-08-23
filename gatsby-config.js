bfc67171f47bf54df5c18611f4e0f85494ba4c5d
stewiezoolights <84042366+stewiezoolights@users.noreply.github.com>
2021-08-22 20:15:46 -0500

    og:image

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     description: `Yearn-ed Liquidity for Apes!
     When your LP's aren't being used, they aren't earning...
     That's why we Ape them into Yearn Finance until they're needed for swaps!`,
-    image: `src/images/social-preview.jpg`,
+    image: `src/images/social.jpeg`,
     author: `@yapeswap`,
     siteUrl: `https://yape.exchange/`,
     keywords: 'defi, dex, tokens, crypto, yield farming, yeild, dao, lp, liquidity provider, rewards, ape, yape',
