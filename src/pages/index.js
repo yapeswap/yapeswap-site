0694402aaa774ee221020476768e839c43775230
stewiezoolights <84042366+stewiezoolights@users.noreply.github.com>
2021-08-22 22:32:43 -0500

    og:image

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,10 @@ import Ape from "../components/Ape"
 import Hero from "../components/Hero"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import Social from "../assets/images/social-preview.jpg"
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Yape!">  
-    <img src={Social} alt='Logo'/>
-    </Seo>
+    <Seo title="Yape!" />  
     <Hero />
     <Ape />
   </Layout>
