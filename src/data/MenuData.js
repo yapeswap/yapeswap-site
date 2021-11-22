bc1b6ea2ed64d4ecccc0e6d8573cc46e5c013c5f
0xSumna <notes@sigil.systems>
2021-11-22 15:58:15 -0600

    Removed Dao Link Temporarily

--- src/data/MenuData.js
+++ src/data/MenuData.js
@@ -1,6 +1,6 @@
 export const menuData = [
     {title: "Doc", link: "https://docs.yape.exchange/"},
-    {title: "DAO", link: "https://dao.yape.exchange/"},
+    // {title: "DAO", link: "https://app.daohaus.club/dao/0x64/0x941c5dec909d8478e748dbad35c67565c91063ba"},
     {title: "Forum", link: "https://forum.yape.exchange/"},
     {title: "Dune Analytics", link: "https://dune.xyz/leakingalphaDOTeth/YapeSwap-Dashboard"},
     
